1c34252e8a12db44cbf2004eb6cd58a26031e43a
Eric Hale <eric_hale@comcast.net>
2020-03-31 10:09:24 -0600

    Add attributes test and fix initialize

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,10 +1,11 @@
 class Apartment
 
-  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms
+  attr_accessor :number, :monthly_rent, :bathrooms, :bedrooms
 
-  def initialize(number, monthly_rent, bathrooms, bedrooms)
+  def initialize(number: , monthly_rent:,  bathrooms:, bedrooms:)
     @number = number
     @monthly_rent = monthly_rent
     @bathrooms = bathrooms
     @bedrooms = bedrooms
+  end
 end
