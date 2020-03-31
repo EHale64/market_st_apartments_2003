815c00d5b13c89770fbf3d7b78b651caa4cce5b2
Eric Hale <eric_hale@comcast.net>
2020-03-31 10:23:53 -0600

    Add Building class and test

---
+++ test/building_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/renter'
+require './lib/apartment'
+require './lib/building'
+require 'pry'
+
+class BuildingTest < Minitest::Test
+end
