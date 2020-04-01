771a5ab1725717f6859458d296a5c345f7f01875
Eric Hale <eric_hale@comcast.net>
2020-04-01 09:42:41 -0600

    Reformatted the Appartment class to properly instatiate to apartment

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,12 +1,17 @@
 class Apartment
 
-  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms, :renter
+  attr_reader :number,
+              :monthly_rent,
+              :bathrooms,
+              :bedrooms,
+              :renter
 
-  def initialize(number: , monthly_rent:,  bathrooms:, bedrooms:)
-    @number = number
-    @monthly_rent = monthly_rent
-    @bathrooms = bathrooms
-    @bedrooms = bedrooms
+  def initialize(apartment)
+    @apartment = apartment
+    @number = apartment[:number]
+    @monthly_rent = apartment[:monthly_rent]
+    @bathrooms = apartment[:bathrooms]
+    @bedrooms = apartment[:bedrooms]
     @renter = nil
   end
 
